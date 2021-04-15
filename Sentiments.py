8415367cfc01a053b7fb12498e07680b72781e19
Sujoya-Datta <81298900+Sujoya-Datta@users.noreply.github.com>
2021-04-15 21:41:31 +0530

    Add files via upload

---
+++ Sentiments.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import sklearn
+import joblib
+model = joblib.load('Tweets')
+st.title('Tweets Classifier')
+ip = st.text_input('Enter your message')
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
